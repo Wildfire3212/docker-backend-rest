e77702d950116de51cfbacd627d09f794dff5fda
Wildfire3212 <oobaquero@comunidad.unam.mx>
2022-02-05 21:48:40 -0600

    intentando arreglar cambios

--- apache_fix.sh
+++ apache_fix.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
-/usr/local/bin/docker-entrypoint.sh apache2-foreground
+sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/apache2.conf
+sed -i "s/VirtualHost \*:80/VirtualHost \*:${PORT:-80}/g" /etc/apache2/sites-available/000-default.conf
--- php.Dockerfile
+++ php.Dockerfile
@@ -1,7 +1,7 @@
 FROM php:7.4.3-apache
 
 COPY ./apache_fix.sh /usr/local/bin
-CMD chmod +x apache_fix.sh \
+CMD chmod +x+r+x apache_fix.sh \
     && [ "apache_fix.sh" ]
 RUN apt update \
         && apt install -y \
